5f292dfc889fbb67b4f342a45a4f43f9529a6667
Juan Niccolini <juan.niccolini@students.snow.edu>
2023-11-30 12:18:19 -0700

    updated cors

--- Program.cs
+++ Program.cs
@@ -3,34 +3,14 @@ using Newtonsoft.Json;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors();
 builder.Services.AddControllers();
-
-// Adding CORS services
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", builder =>
-    {
-        builder.AllowAnyOrigin() // For development, allow requests from any origin
-               .AllowAnyMethod() // Allow all HTTP methods
-               .AllowAnyHeader(); // Allow all headers
-    });
-});
-
-// Other configurations...
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+app.UseCors( x=> x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
-// Use CORS with the specified policy
-app.UseCors("AllowAll");
-
-// Other app configurations...
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
