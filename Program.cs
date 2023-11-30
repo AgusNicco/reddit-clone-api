74e06807a5c0c50b9b19c07a0b663c30c59c92fd
Agus Niccolini <agus@Aguss-MacBook-Pro.local>
2023-11-29 23:51:12 -0700

    api works

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Newtonsoft.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+string json = File.ReadAllText("reddit_posts.json");
+List<RedditPost> posts = JsonConvert.DeserializeObject<List<RedditPost>>(json);
+
+
+app.MapGet("/posts/{index}", (int index) =>
+{
+    index *= 20;
+    int totalPosts = posts.Count - 1;
+    List<RedditPost> postsToReturn = new List<RedditPost>();
+    for (int i = 0; i < 20; i++)
+    {
+        postsToReturn.Add(posts[index + i % totalPosts]);
+    }
+    return Results.Ok(postsToReturn);
+});
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+app.Run();
---
+++ redditpost.cs
@@ -0,0 +1,12 @@
+public class RedditPost
+{
+    public string Author { get; set; }
+    public string Body { get; set; }
+    public string Content { get; set; }
+    public string Id { get; set; }
+    public string NormalizedBody { get; set; }
+    public string Subreddit { get; set; }
+    public string SubredditId { get; set; }
+    public string Summary { get; set; }
+    public int Upvotes { get; set; } 
+}
